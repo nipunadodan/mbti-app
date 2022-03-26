836c8c8d257461ade97771b6afeaf8ab78c95bc4
Nipuna Dodantenna <nipunadodan@gmail.com>
2022-03-26 14:22:29 +0530

    UI tweaks to fit mobile view

--- src/components/comparison/Comparison.js
+++ src/components/comparison/Comparison.js
@@ -27,12 +27,12 @@ const Comparison = () => {
     return (
         <div className={'container mx-auto'}>
             <div className={'flex justify-center flex-col text-center p-8'}>
-                <h1 className={'text-8xl mb-4 inline-block'}>Compare MBTI</h1>
+                <h1 className={'text-5xl md:text-8xl mb-4 inline-block'}>Compare MBTI</h1>
                 <div className={'mt-6'}>
-                    <input className={'w-1/3 border border-gray-400 focus:border-blue-500 outline-0 rounded p-3 mr-3'}
+                    <input className={'md:w-1/3 mt-3 border border-gray-400 focus:border-blue-500 outline-0 rounded p-3 mr-3'}
                           key={'mbti_1'} placeholder={'MBTI Type to Compare 1'} name={'mbti1'} list={'types'}
                           pattern={'[IE]{1}[NS]{1}[TF]{1}[PJ]{1}'} onChange={updateInputs}/>
-                    <input className={'w-1/3 border border-gray-400 focus:border-blue-500 outline-0 rounded p-3 mr-3'}
+                    <input className={'md:w-1/3 mt-3 border border-gray-400 focus:border-blue-500 outline-0 rounded p-3 mr-3'}
                            key={'mbti_2'} placeholder={'MBTI Type to Compare 2'} name={'mbti2'} list={'types'}
                            pattern={'[IE]{1}[NS]{1}[TF]{1}[PJ]{1}'} onChange={updateInputs}/>
                 </div>
@@ -47,7 +47,7 @@ const Comparison = () => {
 
             {
                 (inputs.mbti1 !== '' && inputs.mbti2 !== '')
-                    ?   <div className={'grid grid-cols-2 gap-6 mt-20 mb-8'}>
+                    ?   <div className={'grid md:grid-cols-2 gap-6 mt-20 mb-8 px-3'}>
                             <div className={''}>
                                 <h2 className={'text-6xl'}>{inputs.mbti1}</h2>
                                 {
@@ -55,7 +55,10 @@ const Comparison = () => {
                                         return (
                                             <div className={'my-6'}>
                                                 <h4 className={'font-bold text-2xl'}>{func_desc[x].title} ({x})</h4>
-                                                <p className={'mt-3 text-gray-500 italic border-l-4 border-gray-200 px-2 py-1'}>{func_order[i].name}</p>
+                                                <div className={'mt-3 text-gray-500 italic border-l-4 border-gray-200 px-2 py-1'}>
+                                                    <p>{func_order[i].name}</p>
+                                                    <p className={'text-xs mt-1'}>{func_order[i].desc}</p>
+                                                </div>
                                                 <p className={'py-3'}>{func_desc[x].desc}</p>
                                             </div>
                                         )
@@ -69,7 +72,10 @@ const Comparison = () => {
                                         return (
                                             <div className={'my-6'}>
                                                 <h4 className={'font-bold text-2xl'}>{func_desc[x].title} ({x})</h4>
-                                                <p className={'mt-3 text-gray-500 italic border-l-4 border-gray-200 px-2 py-1'}>{func_order[i].name}</p>
+                                                <div className={'mt-3 text-gray-500 italic border-l-4 border-gray-200 px-2 py-1'}>
+                                                    <p>{func_order[i].name}</p>
+                                                    <p className={'text-xs mt-1'}>{func_order[i].desc}</p>
+                                                </div>
                                                 <p className={'py-3'}>{func_desc[x].desc}</p>
                                             </div>
                                         )
