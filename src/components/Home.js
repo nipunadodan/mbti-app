c1712a817af0a78e290056ba3746e4b3338b0b5d
Nipuna Dodantenna <nipunadodan@gmail.com>
2022-03-28 00:25:03 +0530

    description update

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
       <>
           <Header />
-          <p className={'text-center text-gray-700 dark:text-gray-200 px-6'}>Your favourite MBTI comparison tool has got more functionality and it is a PWA now, means; you can install it as an app and can use completely offline.</p>
+          <p className={'text-center text-gray-700 dark:text-gray-200 px-6'}>Your favourite MBTI comparison tool has got more functionality and it is a PWA now, means; you can install it as an app and can use completely offline. In addition to functional improvements, new version sports a dark mode as well.</p>
           <p>{process.env.REACT_APP_VERSION}</p>
       </>
   )
